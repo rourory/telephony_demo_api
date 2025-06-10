ae1a325577660b91c15773385bc23360bb1df863
rrr <rrr@yandex.by>
2025-06-10 16:11:04 +0300

    password checking test

--- src/app/api/auth/authenticate/route.ts
+++ src/app/api/auth/authenticate/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest) {
 
   if (user.password !== hash) {
     return NextResponse.json(
-      { messages: ["Password incorrect"] },
+      { messages: ["Password incorrect", `${user.password} != ${hash}`] },
       { status: 401 }
     );
   }
