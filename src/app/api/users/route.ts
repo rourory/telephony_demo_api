a8395bb41ad61202cf0ba091c376442793ab4dc5
rrr <rrr@yandex.by>
2025-06-10 13:02:56 +0300

    users get endpoint

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/prisma-clent";
+
+export async function GET(req: NextRequest) {
+  const archived = req.nextUrl.searchParams.get("archived") || "";
+
+  const users = await prisma.administration.findMany({
+    where: {
+      archived: archived === "true",
+    },
+  });
+
+  return NextResponse.json(users);
+}
