b9e8758c6e655dbf58cb2d34a0792f11ca1acda0
rrr <rrr@yandex.by>
2025-06-10 11:39:27 +0300

    first commit

---
+++ src/app/api/administration/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/prisma-clent";
+
+export async function GET() {
+  const administration = prisma.administration.findMany();
+  return NextResponse.json(administration);
+}
