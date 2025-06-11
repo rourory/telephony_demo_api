4e48a270d8d9e05b9c0748ce7be4e7b8af1555d1
rrr <rrr@yandex.by>
2025-06-11 10:58:34 +0300

    devices schema has fixed (ip_device -> ip)

---
+++ src/app/api/settings/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/prisma-clent";
+
+export async function GET(req: NextRequest) {
+  const settings = await prisma.settings.findMany();
+  return NextResponse.json({ data: settings });
+}
